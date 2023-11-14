32b7c5b0731af538c7ec8937b91277054fcb393f
dheeraj_700 <dheeraj700agrahari@gmail.com>
2023-11-14 09:56:37 +0530

    New Changes

--- Maploaction.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    var myLatLng = { lat: 37.7749, lng: -122.4194 }; 
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 10
-    });
-    
-}
